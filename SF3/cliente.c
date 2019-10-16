98651e34c5f303c84c0649575da74fca0c7b2713
jmpram <54644971+jmpram@users.noreply.github.com>
2019-10-16 00:38:23 -0300

    Add files via upload
    modifique un par de cosas para que tengan comunicacion y los sockets queden abiertos.

--- SF3/cliente.c
+++ SF3/cliente.c
@@ -9,49 +9,51 @@ void cargatren(char *tren){
     FILE*archivo=fopen("config.txt","r");
     fgets(tren,20,archivo);
     return;
-    //fscanf(archivo,"%d %d %d %d %d",tren->id, tren->pasajeros,tren->combustible,tren->tViaje,tren->estado);
-}
-int main(int argc, char const *argv[]){
-    /*int seleccion=-1;
-    char comando[25];
-    while(1){
-        memset(comando,'\0',25);
-        printf("indique 1 si desea crear un tren \n");
-        printf("indique 2 si desea salir \n");
-        printf("indique 3 si  \n");
-        scanf("%s\n",comando);
-         
-        switch(seleccion){
-            case 1:    
-      */          //se crea el socket
-                int socketCliente;
-                char buffercv[20];
-                char buffernd[20];
-                //st_tren tren=crearTren();
-            
-                socketCliente=socket(AF_INET, SOCK_STREAM, 0);
-        
-                //especifica una direccion para el socket
-                struct sockaddr_in  direccionDeServidor;
-                direccionDeServidor.sin_family= AF_INET; //tipo de direccion
-                direccionDeServidor.sin_port= htons(5040);  //castea el numero de puerto.
-                direccionDeServidor.sin_addr.s_addr=INADDR_ANY;; // direccion de la ip
-        
-                int estado_Conexion=connect(socketCliente, (struct sockaddr *) &direccionDeServidor, sizeof(direccionDeServidor));
+}//fscanf(archivo,"%d %d %d %d %d",tren->id, tren->pasajeros,tren->combustible,tren->tViaje,tren->estado);
 
-                if(estado_Conexion==-1){
-                    printf("Hubo un error al intentar establecer la conexion\n");
+int main(int argc, char const *argv[]){
+    int op;
+    
+    printf("ingrese un valor para crear tren \n");
+    scanf("%i",&op);
+    char buffercv[20];
+    char buffernd[20];
+    int socketCliente;
+    
+    switch (op){
+        case 1:   
+           
+            socketCliente=socket(AF_INET, SOCK_STREAM, 0);
+        
+            //especifica una direccion para el socket
+            struct sockaddr_in  direccionDeServidor;
+            direccionDeServidor.sin_family= AF_INET; //tipo de direccion
+            direccionDeServidor.sin_port= htons(5040);  //castea el numero de puerto.
+            direccionDeServidor.sin_addr.s_addr=INADDR_ANY;; // direccion de la ip
+        
+            int estado_Conexion=connect(socketCliente, (struct sockaddr *) &direccionDeServidor, sizeof(direccionDeServidor));
+            if(estado_Conexion==-1){
+                printf("Hubo un error al intentar establecer la conexion\n");
+            }   
+            int i=0;
+            //strcpy(buffernd, "aca debería estar el id");
+            while(1){
+        
+        // se recibe los datos del servidor
+                if (i==0){
+                    recv(socketCliente,buffercv,sizeof(char)*20,0);
+                    printf("respuesta del servidor:%s\n",buffercv);
+                    cargatren (&buffernd);
+                    send(socketCliente,buffernd,sizeof(char)*20,0);
+                    //close(socketCliente);
                 }
-                
-           while(1){
-// se recibe los datos del servidor
-                recv(socketCliente,buffercv,sizeof(char)*20,0);
-                printf("respuesta del servidor:%s\n",buffercv);
-                cargatren (&buffernd);
-                send(socketCliente,buffernd,sizeof(char)*20,0);
-                close(socketCliente);
-           }
-     return 0;
-            
+                i=2;
+            }       
+            break;
+        case 2:
+            close(socketCliente);
+            return 0;
+            break;
+    }    
 }    
-       
+      
